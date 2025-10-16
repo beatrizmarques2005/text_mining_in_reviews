8052ddf3089e94942424a7e15b57970dd03f683b
beatrizmarques2005 <beatriz.soares.marques.pt@gmail.com>
2025-10-16 14:34:03 +0100

    organizing repo

---
+++ source/modelling.py
@@ -0,0 +1,7 @@
+"""
+Modelling Module
+---------------------------------------------------
+
+Reusable classes/functions for model training and prediction (e.g., a base Classifier class, functions to train common ML models).
+
+"""
